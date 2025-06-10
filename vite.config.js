23dd36fb5d4a95cb6f2c23032547581d9a1d3f13
Manas Kulkarni <manaskulkarni12345@gmail.com>
2025-06-11 00:42:33 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
+// /* eslint-disable no-undef */
+// import {defineConfig} from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   rules: {
+//     "react/prop-types": 0,
+//   },
+// });
 /* eslint-disable no-undef */
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,7 +27,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  rules: {
-    "react/prop-types": 0,
+  base: "/", // ✅ THIS IS IMPORTANT FOR DEPLOYMENT
+  build: {
+    outDir: "dist", // ✅ make sure Vercel uses this folder
   },
 });
